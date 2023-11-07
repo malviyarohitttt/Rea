edc89b9fc140e7cffd228a3bb818ba1ba891872b
Rohit Malviya <rohitmalviya2324@gmail.com>
2023-11-07 23:20:12 +0530

    React-JS

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CounterCom from './component/counterCom'
+
+export default function App() {
+  return (
+    <div className='container'>
+      <CounterCom/>
+    </div>
+  )
+}
