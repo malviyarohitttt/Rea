f88863f33e788b7bbf6f5dd035109946e2af3b92
Rohit Malviya <rohitmalviya2324@gmail.com>
2023-11-09 23:39:32 +0530

    Student Details Component

--- my-app/src/component/studentDetails.jsx
+++ my-app/src/component/studentDetails.jsx
@@ -13,7 +13,7 @@ export default class studentDetails extends Component {
         branch: "CSE"
       },
       {
-        sname: "rahul",
+        sname: "Rahul",
         email: "rahultmalviya2324@gmail.com",
         age: 21,
         mobile: 8978458789,
@@ -33,45 +33,46 @@ export default class studentDetails extends Component {
     let mobile = document.querySelector("#mobile").value;
     let branch = document.querySelector("#branch").value;
 
-    if ( sname.length < 1 && email.length < 1 && age.length < 1 && mobile.length < 1) {
-      window.alert("Please Fill All The Details");
+    if ( sname.length < 1 || email.length < 1 || age.length < 1 || mobile.length < 1) {
+      window.alert("Please Fill All The Details!");
     } else {
-      {
-        this.state.studentDetails.map((element) => {
-          if (element.mobile == mobile) {
-            window.alert("Student Alredy Exist")
-          }else{
-            const newStudent = { sname, email, age, mobile, branch };
-            this.setState({
-              studentDetails: [...this.state.studentDetails, newStudent],
-            });
-          }
+      const isDuplicate = this.state.studentDetails.map((element) => {
+        return element.mobile == mobile;
+      });
+
+      if (isDuplicate) {
+        window.alert("Student Alredy Exist!");
+      } else {
+        const newStudent = { sname, email, age, mobile, branch };
+        this.setState({
+          studentDetails: [...this.state.studentDetails, newStudent],
         });
       }
-      
     }
   }
   render() {
     return (
       <div className="conatiner mt-5">
-        <h2 className='text-center text-secondary'>Student Details</h2>
+        <h2 className="text-center text-secondary mb-3">Student Details</h2>
         <div className="inputFields mb-5">
-          <input type="text" id="name" className='form-control mt-2 mb-2'placeholder='Enter Your Name'/>
-          <input type="email" id="email"className='form-control mt-2 mb-2' placeholder='Enter Your Email'/>
-          <input type="number" id="age" className='form-control mt-2 mb-2' placeholder='Enter Your Age'/>
-          <input type="number" id="mobile" className='form-control mt-2 mb-2' placeholder='Enter Your Mobile Number'/>
-          <select name="barnch" id="branch" className='form-control mt-2 mb-2'>
-              <option>Select Branch</option>
-              <option value="CSE">CSE</option>
-              <option value="IT">IT</option>
-              <option value="HR">HR</option>
-              <option value="AI">AI</option>
-              <option value="ML">ML</option>
+          <input type="text" id="name" className="form-control mt-2 mb-2" placeholder="Enter Your Name" required/>
+          <input type="email" id="email" className="form-control mt-2 mb-2" placeholder="Enter Your Email" required/>
+          <input type="number" id="age" className="form-control mt-2 mb-2" placeholder="Enter Your Age" required/>
+          <input type="number" id="mobile" className="form-control mt-2 mb-2" placeholder="Enter Your Mobile Number" required/>
+          <select name="barnch" id="branch" className="form-control mt-2 mb-2">
+            <option>Select Branch</option>
+            <option value="CSE">CSE</option>
+            <option value="IT">IT</option>
+            <option value="HR">HR</option>
+            <option value="AI">AI</option>
+            <option value="ML">ML</option>
           </select>
-          <button onClick={this.addStudent} className='btn btn-primary'>Add Student</button>
+          <div className='d-flex justify-content-center'>
+            <button onClick={this.addStudent} className="btn btn-primary">Add Student</button>
+          </div>
         </div>
         <table className="table">
-          <thead>   
+          <thead>
             <tr>
               <th>S.No</th>
               <th>Name</th>
