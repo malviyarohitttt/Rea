64d8a1c061fa6317fdd978683ec8a487b7ee26ca
Rohit Malviya <rohitmalviya2324@gmail.com>
2023-11-09 13:33:53 +0530

    React Js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CounterCom from './component/counterCom'
+import ToDoList from './component/toDoList'
 
 export default function App() {
   return (
     <div className='container'>
-      <CounterCom/>
+      {/* <CounterCom/> */}
+      <ToDoList/>
     </div>
   )
 }
